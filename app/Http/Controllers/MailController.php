971dd32f1b2b3ca727a2bae1f58db6e4fd96e44d
Richardopy <richar.saucedo@gmail.com>
2018-06-22 20:18:04 -0400

    add name in contact

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,12 +13,14 @@ class MailController extends Controller
 
     public function post(Request $request) {
     	$request->validate([
-    			'email' => 'required',
+    			'nombre' => 'required',
+                'email',
     			'subject' => 'required',
-                'celular',
+                'celular' => 'required',
     			'message' => 'required'
     		]);
     	$data = [
+            'nombre'=>$request->nombre,
     		'email'=>$request->email,
     		'subject'=>$request->subject,
             'celular'=>$request->celular,
@@ -29,6 +31,7 @@ class MailController extends Controller
     	Mail::send('mails.mailsend', $data, function($message) use ($data) {
     		$message->from($data['email'], 'Fashion Caacupe');
     		$message->to('techcirclepy@gmail.com');
+            $message->priority($data['nombre']);
             $message->priority($data['celular']);
     		$message->subject($data['subject']);
     	});
