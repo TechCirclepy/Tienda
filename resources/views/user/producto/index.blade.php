1a74e391e517ed01cd49e496daab47ff4b441db9
Hernan <hernachaparro@gmail.com>
2018-06-26 12:50:50 -0400

    modal-productos

--- resources/views/user/producto/index.blade.php
+++ resources/views/user/producto/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.user')
 @section('principal')
-
 <!-- Container (Productos Sección) -->
-
 @include('user.producto.search')
 <div class="row">
     @foreach($productos as $pro)
     @if($pro->emp_activo==1)
-    <div class="col-lg-3 col-sm-3 portfolio-item">
+    <div class="col-lg-3 col-sm-3 portfolio-item" id="{{$pro->pro_id}}">
         <div class="card h-100">
-            <a href="#"><img class="card-img-top img-rounded" src="{{asset('imagenes/productos/'.$pro->pro_foto)}}"  height="200px" alt=""></a>
+            <a href="#">
+                <img class="card-img-top img-rounded open-modal" src="{{asset('imagenes/productos/'.$pro->pro_foto)}}"  height="200px" alt="" data-toggle="modal" data-target="#DetalleModal" 
+                data-pro-id="{{$pro->pro_id}}" data-pro-nom="{{$pro->pro_nom}}" data-pro-info="{{$pro->pro_info}}" data-pro-foto="{{asset('imagenes/productos/'.$pro->pro_foto)}}" data-pro-empresa="{{$pro->empresa}}" data-pro-oferta="{{$pro->pro_oferta}}" data-pro-precio="{{$pro->pro_precio}}" 
+                data-pro-active="{{$pro->pro_ofer_active}}">
+            </a>
             <div class="card-body">
                 <h4 class="card-title">
                     <a href="#">{{$pro->pro_nom}}</a>
@@ -30,10 +32,10 @@
             </div>
         </div>
     </div>
-    @endif
     @include('user.producto.modal')
+    @endif
     @endforeach
 </div>
-
 {{$productos->render()}}
 @endsection
+
