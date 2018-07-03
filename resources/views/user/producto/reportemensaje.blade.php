2c0fc3a86d0effc5086a2eac4bd78c8ca9462ad0
Hernan <hernachaparro@gmail.com>
2018-07-03 14:30:09 -0400

    mensaje de busqueda

--- resources/views/user/producto/reportemensaje.blade.php
+++ resources/views/user/producto/reportemensaje.blade.php
@@ -1,14 +1,9 @@
 
-
 @if(session()->has('busqueda-producto'))
 <div class="alert alert-warning mostrar" id="success-alert">
    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
    <strong>Notificacion</strong>{{session()->get('busqueda-producto')}}
 </div>
-@else
-<script>
-	$('.mostrar').addClass('fade');
-</script>
 @endif
 
 @if(session()->has('notify'))
