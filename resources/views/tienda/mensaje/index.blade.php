9e8531cdff3d2b2ef01c37a71707611ecba5bc08
Richardopy <richar.saucedo@gmail.com>
2018-07-09 20:10:15 -0400

    filtrado mensajes mejoras

--- resources/views/tienda/mensaje/index.blade.php
+++ resources/views/tienda/mensaje/index.blade.php
@@ -6,65 +6,55 @@
 			@include('tienda.mensaje.search')
 		</div>
 	</div>
-	<div class="mb-4" id="accordion" role="tablist" aria-multiselectable="true">
-	  	@foreach($mensajes as $men)
-	  	@if (Auth::user()->admin==1)
-	  		<div class="card">
-	          	<div class="card-header" role="tab" id="headingOne">
-	            	<h5 class="mb-0">
-	              		<a data-toggle="collapse" data-parent="#accordion" href="#{{$men->men_id}}" aria-expanded="true" aria-controls="collapseOne">Nueva compra de: {{$men->nombre}}</a>
-	              		@if ($men->leido == 0)
-	              			<kbd style="background-color: red;box-shadow: 0 0 10px 1px #969696;  position:absolute;right:5px;color:white;">Nuevo</kbd>
-	              		@endif	
-	            	</h5>
-	          	</div>
-				<div id="{{$men->men_id}}" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-            		<div class="card-body">
-            			<p>Cliente: {{$men->nombre}}</p>
-            			<p>Contacto: {{$men->celular}}</p>
-						<b>Producto: </b>{{$men->producto}} | <b>Nº Ref.: </b>{{$men->pro_id}}<br />
-						<img src="{{asset('imagenes/productos/'.$men->pro_foto)}}" class="img-responsive" style="text-align: right;" height="50%" width="50%" alt="" />
-						<b>Mensaje: </b>{{$men->mensaje}} <br />
-						<p style="text-align: right;">
-							@if ($men->leido == 0)
-								<button class="btn btn-success">Macar como leído</button>
-							@endif
-							<a href="" data-target="#modal-delete-{{$men->men_id}}" data-toggle="modal"><button class="btn btn-danger">Eliminar</button></a>
-						</p>
-		  			</div>
-		  		</div>
-		  	</div>	
-	  	@elseif ($men->empresaid==Auth::user()->id)
-			<div class="card">
-	          	<div class="card-header" role="tab" id="headingOne">
-	            	<h5 class="mb-0">
-	              		<a data-toggle="collapse" data-parent="#accordion" href="#{{$men->men_id}}" aria-expanded="true" aria-controls="collapseOne">Nueva compra de: {{$men->nombre}}</a>
-	              		@if ($men->leido == 0)
-	              			<kbd style="background-color: red;box-shadow: 0 0 10px 1px #969696;  position:absolute;right:5px;color:white;">Nuevo</kbd>
-	              		@endif
-	            	</h5>
-	          	</div>
-				<div id="{{$men->men_id}}" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-            		<div class="card-body">
-            			<p>Cliente: {{$men->nombre}}</p>
-            			<p>Contacto: {{$men->celular}}</p>
-						<b>Producto: </b>{{$men->producto}} | <b>Nº Ref.: </b>{{$men->pro_id}}<br />
-						<img src="{{asset('imagenes/productos/'.$men->pro_foto)}}" class="img-responsive" height="25%" width="25%" alt="" />
-						<b>Mensaje: </b>{{$men->mensaje}} <br />
-						<p style="text-align: right;">
-							@if ($men->leido == 0)
-								<button class="btn btn-success" onclick="marcar_leido(this)" data="{{$men->men_id}}">Macar como leído</button>
-							@endif
-							<a href="" data-target="#modal-delete-{{$men->men_id}}" data-toggle="modal"><button class="btn btn-danger">Eliminar</button></a>
-						</p>
-		  			</div>
-		  		</div>
-		  	</div>	
-		@endif	
-		@include('tienda.mensaje.modal')
+
+	<div class="tab">
+	  	@foreach($mensajes as $men)	
+	  		@if ($men->empresaid==Auth::user()->id)
+		  		<button class="tablinks" onclick="openMsg(event, {{$men->men_id}})" id="defaultOpen">
+		  			Nueva compra de: {{$men->nombre}}
+		  			@if ($men->leido == 0)
+	              		<kbd class="{{$men->men_id}}" style="background-color: red; box-shadow: 0 0 10px 1px #969696; color:white;"> Nuevo </kbd>
+	              	@endif
+		  		</button>
+		  	@endif
 		@endforeach
 	</div>
+	@foreach($mensajes as $men)	
+	  	@if ($men->empresaid==Auth::user()->id)
+			<div id="{{$men->men_id}}" class="tabcontent">
+		  		<p>Cliente: {{$men->nombre}}</p>
+    			<p>Contacto: {{$men->celular}}</p>
+				<b>Producto: </b>{{$men->producto}} | <b>Nº Ref.: </b>{{$men->pro_id}}<br />
+				<img src="{{asset('imagenes/productos/'.$men->pro_foto)}}" class="img-responsive" height="25%" width="25%" alt="" />
+				<b>Mensaje: </b>{{$men->mensaje}} <br />
+				<p style="text-align: right;">
+					@if ($men->leido == 0)
+						<button class="btn btn-success" onclick="marcar_leido(this)" data="{{$men->men_id}}">Macar como leído</button>
+					@endif
+					<a href="" data-target="#modal-delete-{{$men->men_id}}" data-toggle="modal"><button class="btn btn-danger">Eliminar</button></a>
+				</p>
+			</div>
+		@endif
+		@include('tienda.mensaje.modal')
+	@endforeach
 	<script>
+		function openMsg(evt, Msg) {
+		    var i, tabcontent, tablinks;
+		    tabcontent = document.getElementsByClassName("tabcontent");
+		    for (i = 0; i < tabcontent.length; i++) {
+		        tabcontent[i].style.display = "none";
+		    }
+		    tablinks = document.getElementsByClassName("tablinks");
+		    for (i = 0; i < tablinks.length; i++) {
+		        tablinks[i].className = tablinks[i].className.replace(" active", "");
+		    }
+		    document.getElementById(Msg).style.display = "block";
+		    evt.currentTarget.className += " active";
+		}
+
+		// Get the element with id="defaultOpen" and click on it
+		document.getElementById("defaultOpen").click();
+		
 		function marcar_leido(e) {
 		    var token = '{{Session::token()}}';
 		    var id_prod = $(e).attr('data');
@@ -88,6 +78,8 @@
 		        dataType: 'json',
 		        success: function (data) {
 		            $(e).hide('slow');
+		            var cambiar = "."+id_prod;
+		            $(cambiar).hide('slow');
 		        },
 		        error: function (data) {
 		            console.log('Error:', data);
